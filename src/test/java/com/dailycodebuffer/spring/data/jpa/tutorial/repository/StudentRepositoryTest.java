ba7ef07308eca3caf55025dacd3d8ef891e21357
clemens2206 <clekerber@tsn.at>
2021-10-26 19:20:09 +0200

    auslagern von Guardian + saveStudentWithGuardian

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
 
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Guardian;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 
 @SpringBootTest
@@ -19,11 +22,35 @@ class StudentRepositoryTest {
                 .emailId("clekerber@gmail.com")
                 .firstName("Clemens")
                 .lastName("Kerber")
-                .guardianName("Hansi")
-                .guardianEmail("hansi@gmail.com")
-                .guardianMobile("99999999999")
+                //.guardianName("Hansi")
+                //.guardianEmail("hansi@gmail.com")
+               // .guardianMobile("99999999999")
+                .build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+
+        Guardian guardian = Guardian.builder()
+                .email("hansi@gmail.com")
+                .mobile("99999994599")
+                .name("Hansi")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Johannes")
+                .emailId("johannes@gmail.com")
+                .lastName("Kerber")
+                .guardian(guardian)
                 .build();
 
         studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println("studentList = " + studentList);
     }
 }
