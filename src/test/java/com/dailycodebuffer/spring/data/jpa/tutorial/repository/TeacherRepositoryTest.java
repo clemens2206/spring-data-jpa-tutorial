73c8f430c40d2e784a9f5ee59c1fe58605995261
clemens2206 <clekerber@tsn.at>
2021-10-28 15:22:45 +0200

    Test Methode saveTeacher

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/TeacherRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/TeacherRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
 
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Teacher;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,4 +16,26 @@ class TeacherRepositoryTest {
     @Autowired
     private TeacherRepository teacherRepository;
 
+
+    @Test
+    public void saveTeacher(){
+
+        Course courseDBA = Course.builder()
+                .titel("DBA")
+                .credit(5)
+                .build();
+
+        Course courseJava = Course.builder()
+                .titel("Java")
+                .credit(6)
+                .build();
+
+        Teacher teacher = Teacher.builder()
+                .firstName("Qutob")
+                .lastName("Kahn")
+                .courses(List.of(courseDBA, courseJava))
+                .build();
+
+        teacherRepository.save(teacher);
+    }
 }
